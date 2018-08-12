44178073f5e5630fa58d76b9ffae624ba100280b
Ferenc Hechler <github@fh.anderemails.de>
2018-08-12 16:07:35 +0200

    familienmitglieder eingeführt.

--- FerisFirstPythonTest.py
+++ FerisFirstPythonTest.py
@@ -1,2 +1,8 @@
-name = input("Wie ist Dein Name? ")
-print("Hallo " + name + " schön Dich zu sehen")
+name = "?"
+familie = ["franzi", "feri", "pati", "katrin"]
+while name != "bye":
+    name = input("Wie ist Dein Name? ")
+    print("Hallo " + name + " schön Dich zu sehen")
+    if name in familie:
+        print("Du bist dran mit Tisch abräumen!")
+print("und tschüss")
